eb67eec2d699dae70a3a238973d6838bfc3b17d0
Byron Fanti <bfant@mybce.catholic.edu.au>
2022-10-13 14:22:56 +1000

    updated dealer.py with comments

--- dealer.py
+++ dealer.py
@@ -7,7 +7,6 @@ import random
 import socket
 
 
-
 host_sock = ('127.0.0.1', 3000) # bind socket
 dealer_sock = socket.socket()
 dealer_sock.bind(host_sock)
@@ -15,13 +14,13 @@ dealer_sock.bind(host_sock)
 server = action.server(threading.Lock()) # server class
 
 
-cards = rules.cards() # card data
-card_data = cards.new()
+cards = rules.cards() # class for card data
+card_data = cards.new() # creates new card data
 dealer = action.dealer(card_data) # init dealer class
 
 
 deck = ds.array(52) # stack for the deck; simulating actual deck of cards
-for key in card_data.keys():
+for key in card_data.keys(): # 
     if key != "cardback":
         deck.apnd(key)
 
@@ -33,15 +32,18 @@ p = int(input("How many people (excluding you) will be connecting? "))
 hands, new_deck = dealer.deal(deck.array, p)
 deck.reinit(new_deck)
 
-dealer_deck = hands.pop()
+dealer_deck = hands.pop() # pop out the dealers deck
 
-iteration = 0
-while iteration < p:
-    dealer_sock.listen(1)
-    conn, addr = dealer_sock.accept()
-    
-    server.lock.acquire()
-    print(f"new connection: {addr}")
-    start_new_thread(server.handle_connection, (conn, hands.pop(), dealer_deck[0])) 
+iteration = 0 # iteration
+while True:
+    if iteration < p: 
+        dealer_sock.listen(1)
+        conn, addr = dealer_sock.accept()
+        
+        server.lock.acquire()
+        print(f"new connection: {addr}")
+        start_new_thread(server.handle_connection, (conn, hands.pop(), dealer_deck[0])) 
+    else:
+       break  
 
 dealer_sock.close()
